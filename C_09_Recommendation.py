4da77af8cc9f5dd37440aa429886ad0a45781ad3
Yoda-Yoda-Yoda <chambersl3255@masseyhigh.school.nz>
2026-05-22 09:20:26 +1200

    starting adding code for price recommend function

--- C_09_Recommendation.py
+++ C_09_Recommendation.py
@@ -1,2 +1,99 @@
-def recommend():
+def num_check(question):
+    """checks that the input the user entered is a number more than 0"""
+    while True:
+        response = input(question).lower()
+
+        if response == "xxx":
+            return response
+
+        try:
+            response = float(response)
+
+            if response > 0:
+                return response
+            elif response < 0:
+                error = "Please enter a positive number"
+                print(error)
+        except ValueError:
+            print("Please enter numbers (Not Letters!)")
+
+def not_blank(question):
+    """Checks that a user input isn't blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry! this can't be blank. Please try again\n")
+
+def unit(question, valid_ans=("ml", "l", "g", "kg", "ea")):
+    """Takes the user answer and checks if it's a valid answer from the list!"""
+    while True:
+        response = input(question).lower()
+
+        for item in valid_ans:
+            if response == item:
+                return item
+
+        if response == "":
+            return "ea"
+        else:
+            print("error")
+
+def unit_converter(item_weight, items_unit):
+    """convert units"""
+    if items_unit == "ml" or items_unit == "g":
+        print("you are in the ml / g loop")
+        item_weight = item_weight / 1000
+
+    return item_weight
+
+def price_calculator(weight1, unit_price):
+    """Calculates the cost per kg/L from the unit cost"""
+    per_kg_l = unit_price / weight1
+    return per_kg_l
+
+def recommend(item_name_1, item_price_unit, budget_1):
     """Recommend the best value for money"""
+    print(item_price_unit)
+    lowest_price = min(item_price_unit)
+    return lowest_price
+
+
+# List to hold price details
+all_item_name = []
+all_item_weight = []
+all_item_cost = []
+all_item_unit = []
+all_item_per_kg = []
+
+price_comparison_dict = {
+    'Name': all_item_name,
+    'Weight': all_item_weight,
+    'Cost': all_item_cost,
+    'Unit': all_item_unit,
+    'per_unit': all_item_per_kg
+}
+budget = num_check("What is your budget? ")
+while True:
+    item_name = not_blank("What is the item name? ")
+    if item_name == "xxx":
+        break
+    weight = num_check("What is the weight? ")
+    unit_question = unit("what is the unit? ")
+    item_cost = num_check("What is the item cost? ")
+    unit_test = unit_converter(weight, unit_question)
+    # changes mL / g to L / KG
+    price_per_weight = price_calculator(unit_test, item_cost)
+
+
+    all_item_name.append(item_name)
+    all_item_weight.append(weight)
+    all_item_cost.append(item_cost)
+    all_item_unit.append(unit_question)
+    all_item_per_kg.append(price_per_weight)
+
+recommendation = recommend(all_item_name, all_item_per_kg, budget)
+print(recommendation)
